c42f89ae77064f43ea9784fe650f493c1241c5ea
glenyr <gleny.rodriguez@live.com>
2018-02-14 16:40:39 -0600

    Fix url

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template lang=pug>
 
 #app
-  img(src='./assets/logo.png')
+  img(src='https://glenyrr.github.io/VueSimpleTest/dist/logo.png')
   h1 PlatziMusic
   select(v-model="selectedCountry")
     option(v-for="country in countries" v-bind:value="country.value") {{country.name}}
